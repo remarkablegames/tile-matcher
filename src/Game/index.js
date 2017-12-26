0b7dfaff71d98c8f0ea8cfc82d7c2c1cb20dcb2e
Mark <mark@remarkablemark.org>
2017-12-25 21:56:22 -0500

    feat(corner): add github corner which links to repository
    https://github.com/tholman/github-corners

--- src/Game/index.js
+++ src/Game/index.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import GitHubCorner from '../GitHubCorner';
 import TileMatcher from '../TileMatcher';
 import tiles from '../helpers/tiles';
 import './style.css';
@@ -6,9 +7,12 @@ import './style.css';
 export default class Game extends Component {
   render() {
     return (
-      <div className="center">
-        <h1 className="heading text-center">Tile Matcher</h1>
-        <TileMatcher tiles={tiles} rowSize={5} columnSize={5} />
+      <div>
+        <GitHubCorner />
+        <div className="center">
+          <h1 className="heading text-center">Tile Matcher</h1>
+          <TileMatcher tiles={tiles} rowSize={5} columnSize={5} />
+        </div>
       </div>
     );
   }
