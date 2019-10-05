b433e44279afa098eed05411a06f38266154e08d
Mark <mark@remarkablemark.org>
2019-10-05 16:20:27 -0400

    refactor: tidy `index.js`

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-const initApp = () => {
-  // initialize google analytics
+import { h, render } from 'preact';
+import Game from './Game';
+import './index.css';
+
+/**
+ * Initializes app.
+ */
+const bootstrap = () => {
+  // initialize google analytics and render game
   require('./helpers/ga');
-
-  // render game
-  const { h, render } = require('preact');
-  const Game = require('./Game').default;
-  require('./index.css');
-
-  render(<Game />, document.getElementById('container'));
+  render(<Game />, document.getElementById('root'));
 };
 
-// try to load polyfill before app
-window.requirejs(
-  ['https://cdn.polyfill.io/v2/polyfill.min.js?features=es5&flags=gated'],
-  initApp,
-  initApp // http://requirejs.org/docs/api.html#errbacks
-);
+/**
+ * Try to load polyfill before app.
+ */
+if (process.env.NODE_ENV === 'development') {
+  bootstrap();
+} else {
+  window.requirejs(
+    ['https://cdn.polyfill.io/v2/polyfill.min.js?features=es5&flags=gated'],
+    bootstrap,
+    bootstrap // http://requirejs.org/docs/api.html#errbacks
+  );
+}
